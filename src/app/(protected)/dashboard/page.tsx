b1f79b701bc6d5cef540be70d8cfe7d5d019b47a
Eduardo Bittencourt <eduardobittencourt08@gmail.com>
2024-10-26 11:20:16 -0300

    feat: logout action

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,17 +1,8 @@
-import { deleteSession } from "@/auth/session";
-import { redirect } from "next/navigation";
+import { action } from "./action";
 
 export default async function DashboardPage() {
-  async function logout() {
-    "use server";
-
-    await deleteSession();
-
-    redirect("/");
-  }
-
   return (
-    <form action={logout}>
+    <form action={action}>
       <button type="submit">Logout</button>
     </form>
   );
